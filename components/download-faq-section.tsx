dca17f05f185df6a07d5e9b670385ac402738c44
isaac844966 <isaackeyz55@gmail.com>
2025-07-23 16:41:32 +0100

    add

--- components/download-faq-section.tsx
+++ components/download-faq-section.tsx
@@ -60,7 +60,7 @@ export default function HowItWorksAndFAQSection() {
           How It Works
         </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-16">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-16">
           {steps.map((step) => (
             <div
               key={step.number}
