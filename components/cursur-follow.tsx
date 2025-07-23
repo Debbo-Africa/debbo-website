7da877ca81c6c527e6e8f0cf2101907fcabd9eec
isaac844966 <isaackeyz55@gmail.com>
2025-07-23 15:24:25 +0100

    add

--- components/cursur-follow.tsx
+++ components/cursur-follow.tsx
@@ -27,7 +27,7 @@ export default function CursorFollower() {
 
   return (
     <div
-      className={`hidden md:fixed w-4 h-4 rounded-full bg-yellow pointer-events-none transition-opacity duration-100 ease-out z-[9999] ${
+      className={`hiddem md:block fixed w-4 h-4 rounded-full bg-yellow pointer-events-none transition-opacity duration-100 ease-out z-[9999] ${
         isVisible ? "opacity-100" : "opacity-0"
       }`}
       style={{
