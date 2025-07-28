6cb2b87b73ad45202abc1d99e1a87ca6ca3b1651
isaac844966 <isaackeyz55@gmail.com>
2025-07-28 14:31:32 +0100

    ad

--- components/Trust.tsx
+++ components/Trust.tsx
@@ -178,9 +178,8 @@ export const Trust = () => {
       <div className=" mx-auto px-4 sm:px-8 relative z-20 pt-20 bg-[--surface-card]">
         <div className="text-center mb-8">
           <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-general-black mb-6 max-w-5xl mx-auto">
-            We focus on conditions that
-            
-            significantly impact African women
+            We focus on the often overlooked but vital health needs of African
+            women
           </h2>
           <p className="text-base sm:text-lg lg:text-xl text-body-text-gray mb-8 max-w-2xl mx-auto px-4">
             Whether you're managing pain, planning a family, or just not feeling
