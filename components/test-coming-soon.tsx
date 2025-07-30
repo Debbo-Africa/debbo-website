575a7ae838d91a3efcc707679aa5021c7fc9d626
isaac844966 <isaackeyz55@gmail.com>
2025-07-30 12:40:56 +0100

    s

--- components/test-coming-soon.tsx
+++ components/test-coming-soon.tsx
@@ -6,9 +6,10 @@ import Link from "next/link";
 const ComingSoon = () => {
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mb-6 items-start">
-        <div className="overflow-hidden min-h-[245px]  md:min-h-[445px] text-center rounded-3xl bg-secondary-debbo1 group p-6 pb-0">
-          <span className="text-white bg-[#FFF5E94D]  w-fit mx-auto px-3 py-1 text-sm md:text-md rounded-full font-semibold mb-6">
+      <div className="grid grid-cols-1 lg:grid-cols-5 gap-6 mb-6 items-start">
+        {/* Left Card: 40% (2/5) */}
+        <div className="lg:col-span-2 overflow-hidden min-h-[245px] md:min-h-[445px] text-center rounded-3xl bg-secondary-debbo1 group p-6 pb-0">
+          <span className="text-white bg-[#FFF5E94D] w-fit mx-auto px-3 py-1 text-sm md:text-md rounded-full font-semibold mb-6">
             Coming Soon
           </span>
           <p className="text-white/90 leading-relaxed max-w-xs mx-auto mb-6 mt-4">
@@ -25,22 +26,26 @@ const ComingSoon = () => {
           </div>
         </div>
 
-        <div className="overflow-hidden md:bg-[url('/images/coming-soon-bg.png')] bg-cover bg-center rounded-3xl p-6 pb-0  md:min-h-[455px]">
-          <div className="flex flex-col items-center md:items-start justify-between h-full text-center md:text-left ">
+        {/* Right Card: 60% (3/5) */}
+        <div className="lg:col-span-3 overflow-hidden md:bg-[url('/images/coming-soon-bg.png')] bg-cover bg-center rounded-3xl p-6 pb-0 md:min-h-[455px]">
+          <div className="flex flex-col items-center md:items-start justify-between h-full text-center md:text-left">
             <div>
               <h2 className="text-2xl md:text-3xl font-semibold mb-4 max-w-xs md:text-white text-general-black">
                 Access Your Test Results
               </h2>
-              <p className="md:text-white text-body-text-gray leading-relaxed max-w-xs mb-6 ">
+              <p className="md:text-white text-body-text-gray leading-relaxed max-w-xs mb-6">
                 Log in to our secure patient portal to view your results
                 anytime, anywhere.
               </p>
             </div>
-            <Link href="https://celldiagnosticslimited.com/login/" target="_blank" >
+            <Link
+              href="https://celldiagnosticslimited.com/login/"
+              target="_blank"
+            >
               <ButtonComponent
                 text="Login Now"
                 linkTo=""
-                className="w-fit md:mt-32 "
+                className="w-fit md:mt-32"
               />
             </Link>
           </div>
