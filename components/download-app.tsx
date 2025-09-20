980aa66cbae26cdbca4761cc1613f5457afd4753
isaac844966 <isaackeyz55@gmail.com>
2025-09-20 09:50:38 +0100

    updated download component to show coming soon

--- components/download-app.tsx
+++ components/download-app.tsx
@@ -38,24 +38,24 @@ export const DownloadSection = () => {
   return (
     <section className="px-4 mx-auto mb-16">
       <div className="space-y-6">
-        {/* <div className="relative rounded-3xl overflow-hidden h-[550px] lg:h-[500px] w-full group">
+        <div className="relative rounded-3xl overflow-hidden h-[550px] lg:h-[500px] w-full group">
           <div className="absolute inset-0 bg-cover bg-center bg-no-repeat bg-[url('/images/app-download-mobile.png')] md:bg-[url('/images/app-download.png')] lg:bg-[url('/images/app-download.png')] transform transition-transform duration-500 ease-in-out group-hover:scale-105" />
 
           <div className="relative z-10 p-6 sm:p-8 lg:p-10 h-full flex flex-col justify-between">
             <div className="flex flex-col sm:flex-row justify-between">
-              <h2 className="text-2xl max-w-[10rem] sm:text-4xl lg:text-5xl font-bold text-general-white mb-4 sm:mb-0 md:leading-loose sm:max-w-xs">
-                Download The MyDébbo App Today
+              <h2 className="text-xl max-w-[10rem] sm:text-4xl lg:text-5xl font-bold text-general-white mb-4 sm:mb-0 md:leading-loose sm:max-w-xs">
+                Coming Soon <br /> Be the first to get started when we launch!
               </h2>
             </div>
 
             <div className="flex items-end md:justify-between justify-center">
-              <ButtonComponent />
+              <ButtonComponent title="Coming Soon " linkTo="" className="w-full md:w-fit"/>
               <div className="bg-general-white rounded-lg p-1.5 sm:p-2 md:flex md:static absolute top-4 right-4">
                 <canvas ref={canvasRef} className="w-24 h-24 lg:w-32 lg:h-32" />
               </div>
             </div>
           </div>
-        </div> */}
+        </div>
 
         <div className="relative rounded-3xl overflow-hidden h-[500px] sm:h-[320px] lg:h-[650px] w-full">
           <video
