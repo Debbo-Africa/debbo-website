6e9fedd35d3cd588a68516a4c4f3664e26957c8b
isaac844966 <isaackeyz55@gmail.com>
2025-09-24 22:43:22 +0100

    updated site map

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default async function sitemap():Promise<MetadataRoute.Sitemap> {
     return [
       {
-        url: `${process.env.NEXT_PUBLIC_SITEURL}/contact-us`,
+        url: `${process.env.NEXT_PUBLIC_SITEURL}`,
       },
     ];
 }
