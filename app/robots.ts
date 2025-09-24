70b439ea2d736705eb964cbdf0b9e87824b0c342
isaac844966 <isaackeyz55@gmail.com>
2025-09-24 22:48:05 +0100

    updated site map

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
-      url: `${process.env.NEXT_PUBLIC_SITEURL}/contact-us`,
+      url: `${process.env.NEXT_PUBLIC_SITEURL}`,
       lastModified: new Date(), 
       changeFrequency: "always", 
       priority: 1,
